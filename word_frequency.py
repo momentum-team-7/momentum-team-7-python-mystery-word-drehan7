54b66e0893e1fcb581ba31180583797ac4e26222
Dawud Rehan <d.rehan7@gmail.com>
2021-02-16 15:58:53 -0500

    seems to be working fine

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,4 @@
+import re
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +8,28 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    regex = re.compile("[^a-zA-Z]")
+    with open(file, 'r') as f:
+        file_words_list = f.read().split()
+
+    lowercase_wordlist = []
+    for word in file_words_list:
+        lowercase_wordlist.append(regex.sub('', word).lower())
+
+    for word in lowercase_wordlist:
+        for stopword in STOP_WORDS:
+            if word == stopword:
+                lowercase_wordlist.remove(word)
+
+    frequency_dict = {}
+    for word in lowercase_wordlist:
+        if word in frequency_dict:
+            frequency_dict[word] += 1
+        else:
+            frequency_dict[word] = 1
+
+    for k, v in frequency_dict.items():
+        print("{:>16} | {:<3} {:<5}".format(k, v, ('*'*v)))
 
 
 if __name__ == "__main__":
